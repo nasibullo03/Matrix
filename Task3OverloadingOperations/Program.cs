852effd4dc2111d5d3b7be21526f09b90b7fa82b
nasibullo03 <nasibullo.khalilov@bk.ru>
2022-03-21 13:08:02 +0700

    Updated the matrix determinant code, fixed errors

--- Task3OverloadingOperations/Program.cs
+++ Task3OverloadingOperations/Program.cs
@@ -2,41 +2,42 @@
 
 namespace Task3OverloadingOperations
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Clear();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Clear();
 
-            SquareMatrix a = new SquareMatrix();
-            SquareMatrix.Size = 4;
-            SquareMatrix.FillAuto (a);
-            Console.WriteLine();
+			SquareMatrix a = new SquareMatrix();
+			SquareMatrix.Size = 4;
+			SquareMatrix.FillAuto (a);
+			Console.WriteLine();
 
-            SquareMatrix b = new SquareMatrix();
-            SquareMatrix.FillAuto (b);
-            Console.WriteLine();
+			SquareMatrix b = new SquareMatrix();
+			SquareMatrix.FillAuto (b);
+			Console.WriteLine();
 
-            // SquareMatrix c = a + b;
-            // c = a - b;
-            // c = a * b;
-            // c = a / b;
-            // bool eq;
-            // eq = a == b;
-            // eq = a != b;
-            // c = a > b;
-            // c = a < b;
-            // c = a >= b;
-            // c = a <= b;
-            // c = a <= b;
-            int hhh = 0;
+			// SquareMatrix c = a + b;
+			// c = a - b;
+			// c = a * b;
+			// c = a / b;
+			// bool eq;
+			// eq = a == b;
+			// eq = a != b;
+			// c = a > b;
+			// c = a < b;
+			// c = a >= b;
+			// c = a <= b;
+			// c = a <= b;
+			int hhh = 0;
 
-            // Console.WriteLine($"Det={SquareMatrix.Determinant (a)}");
-            // Console.WriteLine();
-            // Console.WriteLine(a >= b);
-            hhh = SquareMatrix.DeterminantOver3(a.Matrix);
+			// Console.WriteLine($"Det={SquareMatrix.Determinant (a)}");
+			// Console.WriteLine();
+			// Console.WriteLine(a >= b);
+			hhh = SquareMatrix.DeterminantOver3(a.Matrix);
+			System.Console.WriteLine(hhh);
 
-            Console.ReadLine();
-        }
-    }
+			Console.ReadLine();
+		}
+	}
 }
--- Task3OverloadingOperations/SquareMatrix.cs
+++ Task3OverloadingOperations/SquareMatrix.cs
@@ -4,309 +4,309 @@ using System.Numerics;
 
 namespace Task3OverloadingOperations
 {
-    public class SquareMatrix : IMatrix
-    {
-        private static int size { get; set; }
+	public class SquareMatrix : IMatrix
+	{
+		private static int size { get; set; }
 
-        public static int Size
-        {
-            get
-            {
-                return size;
-            }
-            set
-            {
-                if (value < 1)
-                {
-                    size = 1;
-                }
-                else
-                {
-                    size = value;
-                }
-            }
-        }
+		public static int Size
+		{
+			get
+			{
+				return size;
+			}
+			set
+			{
+				if (value < 1)
+				{
+					size = 1;
+				}
+				else
+				{
+					size = value;
+				}
+			}
+		}
 
-        public int[,] Matrix;
+		public int[,] Matrix;
 
-        public static void FillAuto(SquareMatrix matrix)
-        {
-            IMatrix.FillMatrix(ref matrix, Size);
-        }
+		public static void FillAuto(SquareMatrix matrix)
+		{
+			IMatrix.FillMatrix(ref matrix, Size);
+		}
 
-        public SquareMatrix Copy()
-        {
-            SquareMatrix clone = (SquareMatrix) this.MemberwiseClone();
-            return clone;
-        }
+		public SquareMatrix Copy()
+		{
+			SquareMatrix clone = (SquareMatrix) this.MemberwiseClone();
+			return clone;
+		}
 
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
+		public override bool Equals(object obj)
+		{
+			return base.Equals(obj);
+		}
 
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
 
-        public override string ToString()
-        {
-            return base.ToString();
-        }
+		public override string ToString()
+		{
+			return base.ToString();
+		}
 
-        public static SquareMatrix operator +(SquareMatrix a, SquareMatrix b)
-        {
-            SquareMatrix c = new SquareMatrix();
-            c.Matrix = new int[Size, Size];
-            for (int i = 0; i < Size; ++i)
-            {
-                for (int j = 0; j < Size; ++j)
-                {
-                    c.Matrix[i, j] = a.Matrix[i, j] + b.Matrix[i, j];
-                }
-            }
-            IMatrix.PrintOperations(a, b, "+", c, Size);
-            return c;
-        }
+		public static SquareMatrix operator +(SquareMatrix a, SquareMatrix b)
+		{
+			SquareMatrix c = new SquareMatrix();
+			c.Matrix = new int[Size, Size];
+			for (int i = 0; i < Size; ++i)
+			{
+				for (int j = 0; j < Size; ++j)
+				{
+					c.Matrix[i, j] = a.Matrix[i, j] + b.Matrix[i, j];
+				}
+			}
+			IMatrix.PrintOperations(a, b, "+", c, Size);
+			return c;
+		}
 
-        public static SquareMatrix operator *(SquareMatrix a, SquareMatrix b)
-        {
-            SquareMatrix c = new SquareMatrix();
-            c.Matrix = new int[Size, Size];
-            for (int i = 0; i < Size; ++i)
-            {
-                for (int j = 0; j < Size; ++j)
-                {
-                    c.Matrix[i, j] = a.Matrix[i, j] * b.Matrix[i, j];
-                }
-            }
-            IMatrix.PrintOperations(a, b, "*", c, Size);
-            return c;
-        }
+		public static SquareMatrix operator *(SquareMatrix a, SquareMatrix b)
+		{
+			SquareMatrix c = new SquareMatrix();
+			c.Matrix = new int[Size, Size];
+			for (int i = 0; i < Size; ++i)
+			{
+				for (int j = 0; j < Size; ++j)
+				{
+					c.Matrix[i, j] = a.Matrix[i, j] * b.Matrix[i, j];
+				}
+			}
+			IMatrix.PrintOperations(a, b, "*", c, Size);
+			return c;
+		}
 
-        public static SquareMatrix operator -(SquareMatrix a, SquareMatrix b)
-        {
-            SquareMatrix c = new SquareMatrix();
-            c.Matrix = new int[Size, Size];
-            for (int i = 0; i < Size; ++i)
-            {
-                for (int j = 0; j < Size; ++j)
-                {
-                    c.Matrix[i, j] = a.Matrix[i, j] - b.Matrix[i, j];
-                }
-            }
-            IMatrix.PrintOperations(a, b, "-", c, Size);
-            return c;
-        }
+		public static SquareMatrix operator -(SquareMatrix a, SquareMatrix b)
+		{
+			SquareMatrix c = new SquareMatrix();
+			c.Matrix = new int[Size, Size];
+			for (int i = 0; i < Size; ++i)
+			{
+				for (int j = 0; j < Size; ++j)
+				{
+					c.Matrix[i, j] = a.Matrix[i, j] - b.Matrix[i, j];
+				}
+			}
+			IMatrix.PrintOperations(a, b, "-", c, Size);
+			return c;
+		}
 
-        public static SquareMatrix operator /(SquareMatrix a, SquareMatrix b)
-        {
-            SquareMatrix c = new SquareMatrix();
-            c.Matrix = new int[Size, Size];
-            for (int i = 0; i < Size; ++i)
-            {
-                for (int j = 0; j < Size; ++j)
-                {
-                    try
-                    {
-                        c.Matrix[i, j] = a.Matrix[i, j] / b.Matrix[i, j];
-                    }
-                    catch
-                    {
-                        c.Matrix[i, j] = 0;
-                    }
-                }
-            }
-            IMatrix.PrintOperations(a, b, "/", c, Size);
-            return c;
-        }
+		public static SquareMatrix operator /(SquareMatrix a, SquareMatrix b)
+		{
+			SquareMatrix c = new SquareMatrix();
+			c.Matrix = new int[Size, Size];
+			for (int i = 0; i < Size; ++i)
+			{
+				for (int j = 0; j < Size; ++j)
+				{
+					try
+					{
+						c.Matrix[i, j] = a.Matrix[i, j] / b.Matrix[i, j];
+					}
+					catch
+					{
+						c.Matrix[i, j] = 0;
+					}
+				}
+			}
+			IMatrix.PrintOperations(a, b, "/", c, Size);
+			return c;
+		}
 
-        public static bool operator ==(SquareMatrix a, SquareMatrix b)
-        {
-            int[,] arrA = a.Matrix;
-            int[,] arrB = b.Matrix;
+		public static bool operator ==(SquareMatrix a, SquareMatrix b)
+		{
+			int[,] arrA = a.Matrix;
+			int[,] arrB = b.Matrix;
 
-            if (arrA.GetLength(0) != arrB.GetLength(0))
-            {
-                IMatrix.PrintOperations(a, b, "==", false, Size);
-                return false;
-            }
-            if (arrA.GetLength(1) != arrB.GetLength(1))
-            {
-                IMatrix.PrintOperations(a, b, "==", false, Size);
-                return false;
-            }
+			if (arrA.GetLength(0) != arrB.GetLength(0))
+			{
+				IMatrix.PrintOperations(a, b, "==", false, Size);
+				return false;
+			}
+			if (arrA.GetLength(1) != arrB.GetLength(1))
+			{
+				IMatrix.PrintOperations(a, b, "==", false, Size);
+				return false;
+			}
 
-            for (int i = 0; i < arrA.GetLength(0); i++)
-            {
-                for (int j = 0; j < arrA.GetLength(1); j++)
-                {
-                    if (arrA[i, j] != arrB[i, j])
-                    {
-                        IMatrix.PrintOperations(a, b, "==", false, Size);
-                        return false;
-                    }
-                }
-            }
-            IMatrix.PrintOperations(a, b, "==", true, Size);
-            return true;
-        }
+			for (int i = 0; i < arrA.GetLength(0); i++)
+			{
+				for (int j = 0; j < arrA.GetLength(1); j++)
+				{
+					if (arrA[i, j] != arrB[i, j])
+					{
+						IMatrix.PrintOperations(a, b, "==", false, Size);
+						return false;
+					}
+				}
+			}
+			IMatrix.PrintOperations(a, b, "==", true, Size);
+			return true;
+		}
 
-        public static bool operator !=(SquareMatrix a, SquareMatrix b)
-        {
-            int[,] arrA = a.Matrix;
-            int[,] arrB = b.Matrix;
+		public static bool operator !=(SquareMatrix a, SquareMatrix b)
+		{
+			int[,] arrA = a.Matrix;
+			int[,] arrB = b.Matrix;
 
-            if (arrA.GetLength(0) != arrB.GetLength(0))
-            {
-                IMatrix.PrintOperations(a, b, "!=", true, Size);
-                return true;
-            }
-            if (arrA.GetLength(1) != arrB.GetLength(1))
-            {
-                IMatrix.PrintOperations(a, b, "!=", true, Size);
-                return true;
-            }
+			if (arrA.GetLength(0) != arrB.GetLength(0))
+			{
+				IMatrix.PrintOperations(a, b, "!=", true, Size);
+				return true;
+			}
+			if (arrA.GetLength(1) != arrB.GetLength(1))
+			{
+				IMatrix.PrintOperations(a, b, "!=", true, Size);
+				return true;
+			}
 
-            for (int i = 0; i < arrA.GetLength(0); i++)
-            {
-                for (int j = 0; j < arrA.GetLength(1); j++)
-                {
-                    if (arrA[i, j] != arrB[i, j])
-                    {
-                        IMatrix.PrintOperations(a, b, "!=", true, Size);
-                        return true;
-                    }
-                }
-            }
-            IMatrix.PrintOperations(a, b, "!=", false, Size);
-            return false;
-        }
+			for (int i = 0; i < arrA.GetLength(0); i++)
+			{
+				for (int j = 0; j < arrA.GetLength(1); j++)
+				{
+					if (arrA[i, j] != arrB[i, j])
+					{
+						IMatrix.PrintOperations(a, b, "!=", true, Size);
+						return true;
+					}
+				}
+			}
+			IMatrix.PrintOperations(a, b, "!=", false, Size);
+			return false;
+		}
 
-        public static SquareMatrix operator >(SquareMatrix a, SquareMatrix b)
-        {
-            SquareMatrix c = new SquareMatrix();
-            c.Matrix = new int[Size, Size];
-            for (int i = 0; i < Size; ++i)
-            {
-                for (int j = 0; j < Size; ++j)
-                {
-                    if (a.Matrix[i, j] > b.Matrix[i, j])
-                    {
-                        c.Matrix[i, j] = 1;
-                    }
-                    else
-                    {
-                        c.Matrix[i, j] = 0;
-                    }
-                }
-            }
-            IMatrix.PrintOperations(a, b, ">", c, Size);
-            return c;
-        }
+		public static SquareMatrix operator >(SquareMatrix a, SquareMatrix b)
+		{
+			SquareMatrix c = new SquareMatrix();
+			c.Matrix = new int[Size, Size];
+			for (int i = 0; i < Size; ++i)
+			{
+				for (int j = 0; j < Size; ++j)
+				{
+					if (a.Matrix[i, j] > b.Matrix[i, j])
+					{
+						c.Matrix[i, j] = 1;
+					}
+					else
+					{
+						c.Matrix[i, j] = 0;
+					}
+				}
+			}
+			IMatrix.PrintOperations(a, b, ">", c, Size);
+			return c;
+		}
 
-        public static SquareMatrix operator <(SquareMatrix a, SquareMatrix b)
-        {
-            SquareMatrix c = new SquareMatrix();
-            c.Matrix = new int[Size, Size];
-            for (int i = 0; i < Size; ++i)
-            {
-                for (int j = 0; j < Size; ++j)
-                {
-                    if (a.Matrix[i, j] < b.Matrix[i, j])
-                    {
-                        c.Matrix[i, j] = 1;
-                    }
-                    else
-                    {
-                        c.Matrix[i, j] = 0;
-                    }
-                }
-            }
-            IMatrix.PrintOperations(a, b, "<", c, Size);
-            return c;
-        }
+		public static SquareMatrix operator <(SquareMatrix a, SquareMatrix b)
+		{
+			SquareMatrix c = new SquareMatrix();
+			c.Matrix = new int[Size, Size];
+			for (int i = 0; i < Size; ++i)
+			{
+				for (int j = 0; j < Size; ++j)
+				{
+					if (a.Matrix[i, j] < b.Matrix[i, j])
+					{
+						c.Matrix[i, j] = 1;
+					}
+					else
+					{
+						c.Matrix[i, j] = 0;
+					}
+				}
+			}
+			IMatrix.PrintOperations(a, b, "<", c, Size);
+			return c;
+		}
 
-        public static SquareMatrix operator >=(SquareMatrix a, SquareMatrix b)
-        {
-            SquareMatrix c = new SquareMatrix();
-            c.Matrix = new int[Size, Size];
-            for (int i = 0; i < Size; ++i)
-            {
-                for (int j = 0; j < Size; ++j)
-                {
-                    if (a.Matrix[i, j] >= b.Matrix[i, j])
-                    {
-                        c.Matrix[i, j] = 1;
-                    }
-                    else
-                    {
-                        c.Matrix[i, j] = 0;
-                    }
-                }
-            }
-            IMatrix.PrintOperations(a, b, ">=", c, Size);
-            return c;
-        }
+		public static SquareMatrix operator >=(SquareMatrix a, SquareMatrix b)
+		{
+			SquareMatrix c = new SquareMatrix();
+			c.Matrix = new int[Size, Size];
+			for (int i = 0; i < Size; ++i)
+			{
+				for (int j = 0; j < Size; ++j)
+				{
+					if (a.Matrix[i, j] >= b.Matrix[i, j])
+					{
+						c.Matrix[i, j] = 1;
+					}
+					else
+					{
+						c.Matrix[i, j] = 0;
+					}
+				}
+			}
+			IMatrix.PrintOperations(a, b, ">=", c, Size);
+			return c;
+		}
 
-        public static SquareMatrix operator <=(SquareMatrix a, SquareMatrix b)
-        {
-            SquareMatrix c = new SquareMatrix();
-            c.Matrix = new int[Size, Size];
-            for (int i = 0; i < Size; ++i)
-            {
-                for (int j = 0; j < Size; ++j)
-                {
-                    if (a.Matrix[i, j] <= b.Matrix[i, j])
-                    {
-                        c.Matrix[i, j] = 1;
-                    }
-                    else
-                    {
-                        c.Matrix[i, j] = 0;
-                    }
-                }
-            }
-            IMatrix.PrintOperations(a, b, "<=", c, Size);
-            return c;
-        }
+		public static SquareMatrix operator <=(SquareMatrix a, SquareMatrix b)
+		{
+			SquareMatrix c = new SquareMatrix();
+			c.Matrix = new int[Size, Size];
+			for (int i = 0; i < Size; ++i)
+			{
+				for (int j = 0; j < Size; ++j)
+				{
+					if (a.Matrix[i, j] <= b.Matrix[i, j])
+					{
+						c.Matrix[i, j] = 1;
+					}
+					else
+					{
+						c.Matrix[i, j] = 0;
+					}
+				}
+			}
+			IMatrix.PrintOperations(a, b, "<=", c, Size);
+			return c;
+		}
 
-        private static int Determinant3x3(int[,] m)
-        {
-            int det = default;
-            int
-                a = m[0, 0],
-                b = m[0, 1],
-                c = m[0, 2],
-                d = m[1, 0],
-                e = m[1, 1],
-                f = m[1, 2],
-                g = m[2, 0],
-                h = m[2, 1],
-                i = m[2, 2];
+		private static int Determinant3x3(int[,] m)
+		{
+			int det = default;
+			int
+				a = m[0, 0],
+				b = m[0, 1],
+				c = m[0, 2],
+				d = m[1, 0],
+				e = m[1, 1],
+				f = m[1, 2],
+				g = m[2, 0],
+				h = m[2, 1],
+				i = m[2, 2];
 
-            det =
-                a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
-            return det;
-        }
+			det =
+				a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
+			return det;
+		}
 
-        // TODO: pppp времменая операция  надо потом удалить
-        public static void pppp(int[,] mm)
-        {
-            for (int i = 0; i < mm.GetLength(0); ++i)
-            {
-                for (int j = 0; j < mm.GetLength(0); ++j)
-                {
-                    Console.Write("{0,4}", mm[i, j]);
-                }
-                System.Console.WriteLine();
-            }
-            System.Console.WriteLine();
-        }
+		// TODO: pppp времменая операция  надо потом удалить
+		public static void pppp(int[,] mm)
+		{
+			for (int i = 0; i < mm.GetLength(0); ++i)
+			{
+				for (int j = 0; j < mm.GetLength(0); ++j)
+				{
+					Console.Write("{0,4}", mm[i, j]);
+				}
+				System.Console.WriteLine();
+			}
+			System.Console.WriteLine();
+		}
 
-        public static int DeterminantOver3(int[,] MainMatrix)
-        {
+		public static int DeterminantOver3(int[,] MainMatrix)
+		{
             /*
 			a  b  c
 			d  e  f 
@@ -315,78 +315,82 @@ namespace Task3OverloadingOperations
 			|A| = a(ei − fh) − b(di − fg) + c(dh − eg)
 			
 			*/
-            int
-                DetSize = MainMatrix.GetLength(0) - 1,
-                det = default,
-                RowIndex = 0,
-                ColIndex = 0,
-                MatIndex = MainMatrix.GetLength(0);
+            if (MainMatrix.GetLength(0) == 3)
+			{
+				return Determinant3x3(MainMatrix);
+			}
 
-            int[,] DetMatrix = new int[DetSize, DetSize];
+			int
+				DetSize = MainMatrix.GetLength(0) - 1,
+				det = 0,
+				RowIndex = 0,
+				ColIndex = 0,
+				MatIndex = MainMatrix.GetLength(0);
 
-            pppp (MainMatrix);
+			int[,] DetMatrix = new int[DetSize, DetSize];
 
-            for (int FirstColInd = 0; FirstColInd < MatIndex; ++FirstColInd)
-            {
-                for (
-                    int MatrixColumn = 1;
-                    MatrixColumn < MatIndex;
-                    ++MatrixColumn
-                )
-                {
-                    for (int MatrixRow = 0; MatrixRow < MatIndex; ++MatrixRow)
-                    {
-                        if (MatrixRow == FirstColInd)
-                        {
-                            continue;
-                        }
-                        else
-                        {
-                            DetMatrix[ColIndex, RowIndex++] =
-                                MainMatrix[MatrixColumn, MatrixRow];
+			for (int FirstColInd = 0; FirstColInd < MatIndex; ++FirstColInd)
+			{
+				for (
+					int MatrixColumn = 1;
+					MatrixColumn < MatIndex;
+					++MatrixColumn
+				)
+				{
+					for (int MatrixRow = 0; MatrixRow < MatIndex; ++MatrixRow)
+					{
+						if (MatrixRow == FirstColInd)
+						{
+							continue;
+						}
+						else
+						{
+							DetMatrix[ColIndex, RowIndex++] =
+								MainMatrix[MatrixColumn, MatrixRow];
+						}
+					}
+					RowIndex = 0;
+					++ColIndex;
+				}
+				ColIndex = 0;
+				RowIndex = 0;
+				if (FirstColInd % 2 == 0)
+				{
+					det +=
+						MainMatrix[0, FirstColInd] *
+						DeterminantOver3(DetMatrix);
+				}
+				else if (FirstColInd % 2 == 1)
+				{
+					det -=
+						MainMatrix[0, FirstColInd] *
+						DeterminantOver3(DetMatrix);
+				}
 
-                            if (MatrixRow >= (MatIndex - 1))
-                            {
-                                ++ColIndex;
-                            }
-                            // TODO: неправильно показывает последную матрицу
-                            // TODO: ColIndex и RowIndex не увеличиваются
-                        }
-                    }
-                    RowIndex = 0;
-                }
-                ColIndex = 0;
-                RowIndex = 0;
-                System
-                    .Console
-                    .WriteLine("i = {0}  index = {1}",
-                    MainMatrix[0, FirstColInd],
-                    FirstColInd);
-                pppp (DetMatrix);
-                DetMatrix = new int[DetSize, DetSize];
-            }
-            return det;
-        }
+				DetMatrix = new int[DetSize, DetSize];
+			}
+			return det;
+		}
 
-        public static int Determinant(SquareMatrix m)
-        {
-            int det = default;
+		public static int Determinant(SquareMatrix m)
+		{
+			int det = default;
 
-            if (Size == 2)
-            {
-                det =
-                    m.Matrix[0, 0] * m.Matrix[1, 1] -
-                    m.Matrix[1, 0] * m.Matrix[0, 1];
-            }
-            else if (Size == 3)
-            {
-                det = Determinant3x3(m.Matrix);
-            }
-            else if (Size > 3)
-            {
-                det = DeterminantOver3(m.Matrix);
-            }
-            return det;
-        }
-    }
+			if (Size == 2)
+			{
+				det =
+					m.Matrix[0, 0] * m.Matrix[1, 1] -
+					m.Matrix[1, 0] * m.Matrix[0, 1];
+			}
+			else if (Size == 3)
+			{
+				det = Determinant3x3(m.Matrix);
+			}
+			else if (Size > 3)
+			{
+				det = DeterminantOver3(m.Matrix);
+			}
+			return det;
+		}
+	}
 }
